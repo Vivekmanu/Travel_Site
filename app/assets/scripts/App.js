e66617e24da395d812f856cf67d76f5298fb55a9
Vivek Mohan Singh <vivekmanu1234@gmail.com>
2016-11-25 08:14:22 +0530

    25th Nov.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,5 @@
-import Person from './modules/Person';
+import $ from 'jquery';
 
-class Adult extends Person{
-  payTaxes(){
-    console.log(this.name + "now owes $0 in taxes.");
-  }
-}
+import MobileMenu from  './modules/MobileMenu';
 
-var john = new Person ("John Doe", "blue");
-john.greet();
-
-var jane = new Adult ("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
