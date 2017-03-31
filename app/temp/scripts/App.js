73fbaa3b4a899e1c3adc27e18ad64ee23bbd0941
Vivek Mohan Singh <vivekmanu1234@gmail.com>
2017-03-31 12:33:32 +0530

    completed desktop and mobile menu header styles.

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10324,6 +10324,7 @@
 	        value: function toggleTheMenu() {
 	            this.menuContent.toggleClass("site-header__menu-content--is-visible");
 	            this.siteHeader.toggleClass("site-header--is-expanded");
+	            this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 	        }
 	    }]);
 
